c83b06a3160821cea0bfb3d35bf0891123b1cafb
Eugenio Baglieri <eugenio.baglieri@skylabs.it>
2023-12-13 10:05:00 +0100

    Add further properties to Payer model

--- lib/src/orders/model/payer.dart
+++ lib/src/orders/model/payer.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_paypal_sdk/core.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'payer.g.dart';
@@ -5,13 +6,35 @@ part 'payer.g.dart';
 /// The payer of an order.
 @JsonSerializable(fieldRename: FieldRename.snake)
 class Payer {
+  /// The name of the payer. Supports only the given_name and surname properties.
+  final Name? name;
+
   /// The email address of the payer.
   final String? emailAddress;
+
+  /// The phone number of the customer.
+  /// Available only when you enable the Contact Telephone Number option in the Profile & Settings for the merchant's PayPal account
+  final PhoneWithType? phone;
+
+  /// The birth date of the payer in YYYY-MM-DD format.
+  final String? birthDate;
+
+  /// The address of the payer.
+  /// Supports only the address_line_1, address_line_2, admin_area_1, admin_area_2, postal_code, and country_code properties.
+  /// Also referred to as the billing address of the customer.
+  final AddressPortable? address;
 
   /// The PayPal-assigned ID for the payer.
   final String? payerId;
 
-  const Payer({this.emailAddress, this.payerId});
+  const Payer({
+    this.name,
+    this.emailAddress,
+    this.phone,
+    this.birthDate,
+    this.address,
+    this.payerId,
+  });
 
   Map<String, dynamic> toJson() => _$PayerToJson(this);
 
@@ -19,6 +42,6 @@ class Payer {
 
   @override
   String toString() {
-    return 'Payer{emailAddress: $emailAddress, payerId: $payerId}';
+    return 'Payer(name: $name, emailAddress: $emailAddress, phone: $phone, birthDate: $birthDate, address: $address, payerId: $payerId)';
   }
 }
--- lib/src/orders/model/payer.g.dart
+++ lib/src/orders/model/payer.g.dart
@@ -7,7 +7,17 @@ part of 'payer.dart';
 // **************************************************************************
 
 Payer _$PayerFromJson(Map<String, dynamic> json) => Payer(
+      name: json['name'] == null
+          ? null
+          : Name.fromJson(json['name'] as Map<String, dynamic>),
       emailAddress: json['email_address'] as String?,
+      phone: json['phone'] == null
+          ? null
+          : PhoneWithType.fromJson(json['phone'] as Map<String, dynamic>),
+      birthDate: json['birth_date'] as String?,
+      address: json['address'] == null
+          ? null
+          : AddressPortable.fromJson(json['address'] as Map<String, dynamic>),
       payerId: json['payer_id'] as String?,
     );
 
@@ -20,7 +30,11 @@ Map<String, dynamic> _$PayerToJson(Payer instance) {
     }
   }
 
+  writeNotNull('name', instance.name);
   writeNotNull('email_address', instance.emailAddress);
+  writeNotNull('phone', instance.phone);
+  writeNotNull('birth_date', instance.birthDate);
+  writeNotNull('address', instance.address);
   writeNotNull('payer_id', instance.payerId);
   return val;
 }
